f4c0268f9e49aa265dcc63607d4d0ecb799d65b9
Sarika <2478056@cognizant.com>
2026-04-02 16:08:46 +0530

    Recovered identity service

--- backend/identity-service/src/main/java/com/greencity/identity/IdentityServiceApplication.java
+++ backend/identity-service/src/main/java/com/greencity/identity/IdentityServiceApplication.java
@@ -3,6 +3,7 @@ package com.greencity.identity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class IdentityServiceApplication {
   public static void main(String[] args) {
